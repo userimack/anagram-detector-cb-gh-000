e29bee0dbb431d7803e36f869231ebcc1586f9ac
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-08 16:55:39 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_reader :anagram 
+  
+  def initialize(anagram)
+    @anagram = anagram 
+  end
+  
+  def match(word_list)
+    result_list = []
+    
+    word_list.each do |word|
+      result_list << word if word.split("").sort == @anagram.split("").sort
+    end
+    result_list
+  end 
+  
+end
